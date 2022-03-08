34b64f8ba20cbcba0261f66f9920a48073d50bee
Firaaryunita <99790025+Firaaryunita@users.noreply.github.com>
2022-03-08 15:34:00 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text('Pemrograman Mobile'),
+        ),
+        body: Center(
+          child: Text('Fira aryunita'),
+    ),
+   ),
+ );
+ }
+}
+
+  
